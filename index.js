b17eaedf083d92bb8dbfcb40d22aa4823234fcd1
QiaoqingWu-AC <wu000302@algonquiinlive.com>
2024-10-31 19:38:42 -0400

    Create a nodejs rest application

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Initialize the express application and port number
+const app = express();
+const PORT = 8000;
+
